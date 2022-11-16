15d409b22e109af8000b145337b6271db30d8afa
Rastko Kisjelica <kisjelicarastko@gmail.com>
2022-11-16 02:06:09 +0100

    Bug fix.

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        
+        'api' => [
+            'driver' => 'session', // here !
+            'provider' => 'users',
+        ],
     ],
 
     /*
